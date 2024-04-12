16de4122231b4600f59748ebc4451e6dae557711
Eva Bespalova <evysia6@gmail.com>
2024-04-12 22:16:55 +0500

    in2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -38,8 +38,11 @@ public class Main {
 
     public static boolean hasZeroSides(int[] nums) {
 
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (i!=0 && nums[i - 1] == 0 && nums[i + 1] == 0) {
+        for (int i = 0; i < nums.length - 1; ++i) {
+            if ((i == 0 || i == nums.length - 2) && nums[i] == 0 && nums[i + 1] == 0) {
+                return true;
+            }
+            if (nums[i] == 0 && nums[i + 1] == 0) {
                 return true;
             }
         }
